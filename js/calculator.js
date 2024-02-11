9e282366dbed5ec9883117b27aa78f7a5c78be04
aaron <aluch07@yahoo.com>
2024-02-11 16:40:54 -0500

    Basic outline

--- js/calculator.js
+++ js/calculator.js
@@ -1,9 +1,11 @@
 let displayValue = 0;
 
+// updates calculator's display
 function updateDisplay() {
     document.getElementById("display").textContent=displayValue;
 }
 
+// appends button pressed to display
 function appendDisplay(value) {
     if (displayValue === "0") {
         displayValue = value;
