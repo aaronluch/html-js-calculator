5e73ff1a0678d84a7f05cfe26b2c8e0def9639d9
aaron <aluch07@yahoo.com>
2024-02-11 16:44:12 -0500

    calculator functions
    add more logic

--- js/calculator.js
+++ js/calculator.js
@@ -14,3 +14,14 @@ function appendDisplay(value) {
     }
     updateDisplay();
 }
+
+// clears calculator's display
+function clearDisplay() {
+    displayValue = "0"; // defaults to a 0 when clearing
+    updateDisplay();
+}
+
+function calculateResult() {
+    displayValue = eval(displayValue.toString());
+    updateDisplay();
+}
